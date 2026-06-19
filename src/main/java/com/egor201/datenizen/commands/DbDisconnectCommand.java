ac9f4c2c21721a0d6d1bbb72e48bd1203ffead09
Egor201 <Egorsimikin934@gmail.com>
2026-06-19 18:28:13 +0300

    fix NPE in db_disconnect when id prefix is omitted

--- src/main/java/com/egor201/datenizen/commands/DbDisconnectCommand.java
+++ src/main/java/com/egor201/datenizen/commands/DbDisconnectCommand.java
@@ -17,7 +17,11 @@ public class DbDisconnectCommand extends AbstractCommand {
     // -->
     public DbDisconnectCommand() { setName("db_disconnect"); setSyntax("db_disconnect [id:<id>]"); setRequiredArguments(1, 1); }
     @Override public void parseArgs(ScriptEntry se) throws InvalidArgumentsException {
-        for (Argument arg : se) if (!se.hasObject("id") && arg.matchesPrefix("id")) se.addObject("id", arg.asElement());
+        for (Argument arg : se) {
+            if (!se.hasObject("id") && arg.matchesPrefix("id")) se.addObject("id", arg.asElement());
+            else arg.reportUnhandled();
+        }
+        if (!se.hasObject("id")) throw new InvalidArgumentsException("Must specify id!");
     }
     @Override public void execute(ScriptEntry se) { Datenizen.getInstance().getDatabaseManager().disconnect(se.getElement("id").asString()); }
 }
